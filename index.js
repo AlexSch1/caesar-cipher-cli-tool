744ef25bca96b1f02d4c3c8d8afeac86c30b8ed7
ashulyak <alexey.shulyak@vrpconsulting.com>
2021-05-07 20:56:22 +0300

    feat: add minimist arg

---
+++ index.js
@@ -0,0 +1,15 @@
+const minimist = require('minimist');
+
+const rawArguments = process.argv.slice(2);
+const arguments = minimist(rawArguments, {
+    alias: {
+        s: 'shift',
+        i: 'input',
+        o: 'output',
+        a: 'action'
+    }
+});
+
+console.log('arguments:', arguments);
+
+
