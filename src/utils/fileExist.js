c20d12012411af2d3e4823b5141a350cec1a7e4a
ashulyak <alexey.shulyak@vrpconsulting.com>
2021-05-09 18:09:09 +0300

    refactor: refactor file exist

--- src/utils/fileExist.js
+++ src/utils/fileExist.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 const errorProcessing = require('../utils/errorProcessing');
 
-module.exports = (path, namePath) => {
-  fs.stat(path, (err, stat) => {
-    if (err || !stat.isFile()) {
+module.exports = async (path, namePath) => {
+  try {
+    const stats = fs.statSync(path);
+    if (!stats || !stats.isFile()) {
       errorProcessing(`No such ${namePath} file`);
     }
-  });
+  } catch (e) {
+    errorProcessing(`No such ${namePath} file`);
+  }
 };
